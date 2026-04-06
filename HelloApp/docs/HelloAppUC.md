9f42e3a880500614dc5c95c89ec4162c6079d577
archie14-maker <aaradhyaarchiee@gmail.com>
2026-04-06 11:20:01 +0530

    UC7: Use String.Join fir multiple names

---
+++ HelloApp/docs/HelloAppUC.md
@@ -0,0 +1,5 @@
+## UC7: String.join()
+
+- Uses built-in String.join()
+- No loops required
+- Cleanest implementation
---
+++ HelloApp/src/main/java/helloapp/HelloApp.java
@@ -0,0 +1,16 @@
+package helloapp;
+
+public class HelloApp {
+
+    public static void main(String[] args) {
+
+        String names;
+        if (args.length == 0) {
+            names = "World";
+        } else {
+            names = String.join(", ", args);
+        }
+
+        System.out.println("Hello, " + names + "!");
+    }
+}
